6484a9025014950d2388129ea99e7d97cbe99074
Chen3131 <202311109@fit.edu.ph>
2024-11-22 15:24:12 +0800

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -2,7 +2,13 @@ public class Admin {
     private String admin_username;
     private int admin_pin;
 
+    //default constructor that User will call
     public Admin() {
+        this.admin_pin = 1234;
+        this.admin_username = "admin";
+    }
+
+    public Admin(String admin_username, int admin_pin) {
         this.admin_username = admin_username;
         this.admin_pin = admin_pin;
     }
@@ -16,12 +22,9 @@ public class Admin {
 
     public void setAdminUsername(String admin_username) {
         this.admin_username = admin_username;
-
     }
 
     public void setAdminPin(int admin_pin) {
         this.admin_pin = 1234;
     }
-
-
 }
